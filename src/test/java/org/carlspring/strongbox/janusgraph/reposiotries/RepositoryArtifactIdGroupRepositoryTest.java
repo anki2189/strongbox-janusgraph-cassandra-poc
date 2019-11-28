c906dbb8176c8a6e13fc783911fff0b75b4d615c
sbespalov <serge.bespalov@gmail.com>
2019-11-28 10:51:42 +0700

    ArtifactGroup implementation

--- src/test/java/org/carlspring/strongbox/janusgraph/reposiotries/RepositoryArtifactIdGroupRepositoryTest.java
+++ src/test/java/org/carlspring/strongbox/janusgraph/reposiotries/RepositoryArtifactIdGroupRepositoryTest.java
@@ -7,7 +7,10 @@ import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.carlspring.strongbox.janusgraph.app.Application;
 import org.carlspring.strongbox.janusgraph.domain.Artifact;
 import org.carlspring.strongbox.janusgraph.domain.ArtifactCoordinates;
@@ -35,40 +38,87 @@ public class RepositoryArtifactIdGroupRepositoryTest
     {
         GraphTraversalSource g = janusGraph.traversal();
 
-        String artifactGroupUuid = UUID.randomUUID().toString();
-        g.addV(ArtifactCoordinates.LABEL)
-         .property("uuid", UUID.randomUUID().toString())
-         .property("path",
-                   "org/carlspring/test/findArtifactsByGroupIdShouldWork.jar")
-         .property("version",
-                   "1.2.7")
-         .as("adc1")
-         .addV(Artifact.LABEL)
-         .property("uuid", UUID.randomUUID().toString())
-         .property("storageId", "storage0")
-         .property("repositoryId", "releases")
-         .as("ae1")
-         .addE(Edges.ARTIFACT_ARTIFACTCOORDINATES)
-         .to("adc1")
-         .addV(ArtifactGroup.LABEL)
-         .property("uuid", artifactGroupUuid)
-         .property("groupId", "findArtifactsByGroupIdShouldWorkGroup")
-         .as("ag1")
+        g.inject(1)
+         .map(addArtifactGroup("findArtifactsByGroupIdShouldWorkGroup"))
          .addE(Edges.ARTIFACTGROUP_ARTIFACT)
-         .to("ae1")
-         .addV(RepositoryArtifactIdGroup.LABEL)
-         .property("uuid", artifactGroupUuid)
-         .property("storageId", "storage0")
-         .property("repositoryId", "releases")
-         .addE(Edges.REPOSITORYARTIFACTIDGROUP_ARTIFACTGROUP)
-         .to("ag1")
+         .to(addArtifact("org/carlspring/test/findArtifactsByGroupIdShouldWork.jar",
+                         "1.2.7"))
+         .outV()
+         .addE(Edges.ARTIFACTGROUP_ARTIFACT)
+         .to(addArtifact("org/carlspring/test/findArtifactsByGroupIdShouldWork.jar",
+                         "1.2.8"))
+         .outV()
+         .addE(Edges.ARTIFACTGROUP_ARTIFACT)
+         .to(addArtifact("org/carlspring/test/findArtifactsByGroupIdShouldWork.jar",
+                         "1.2.9"))
+         .next();
+
+        g.inject(1)
+         .map(addArtifactGroup("anotherGroup"))
+         .addE(Edges.ARTIFACTGROUP_ARTIFACT)
+         .to(addArtifact("org/carlspring/test/anotherGroup.jar",
+                         "1.2.7"))
+         .outV()
+         .addE(Edges.ARTIFACTGROUP_ARTIFACT)
+         .to(addArtifact("org/carlspring/test/anotherGroup.jar",
+                         "1.2.8"))
          .next();
 
         g.tx().commit();
-        
+
         List<ArtifactEntity> result = repositoryArtifactIdGroupRepository.findArtifactsByGroupId("storage0", "releases",
                                                                                                  "findArtifactsByGroupIdShouldWorkGroup");
-        assertEquals(1, result.size());
+        assertEquals(3, result.size());
+
+        List<ArtifactEntity> anotherResult = repositoryArtifactIdGroupRepository.findArtifactsByGroupId("storage0",
+                                                                                                        "releases",
+                                                                                                        "anotherGroup");
+        assertEquals(2, anotherResult.size());
+    }
+
+    protected GraphTraversal<Object, Vertex> addArtifactGroup(String groupId)
+    {
+        String artifactGroupUuid = UUID.randomUUID().toString();
+
+        return __.addV(ArtifactGroup.LABEL)
+                 .property("uuid",
+                           artifactGroupUuid)
+                 .property("groupId",
+                           groupId)
+                 .as("ag1")
+                 .addV(RepositoryArtifactIdGroup.LABEL)
+                 .property("uuid",
+                           artifactGroupUuid)
+                 .property("storageId",
+                           "storage0")
+                 .property("repositoryId",
+                           "releases")
+                 .addE(Edges.REPOSITORYARTIFACTIDGROUP_ARTIFACTGROUP)
+                 .to("ag1")
+                 .select("ag1");
+    }
+
+    protected GraphTraversal<Object, Vertex> addArtifact(String path,
+                                                         String version)
+    {
+        String alias = "ae1";
+        String coordinatesAslias = alias + ".coordinates";
+
+        return __.addV(ArtifactCoordinates.LABEL)
+                 .property("uuid", UUID.randomUUID().toString())
+                 .property("path",
+                           path)
+                 .property("version",
+                           version)
+                 .as(coordinatesAslias)
+                 .addV(Artifact.LABEL)
+                 .property("uuid", UUID.randomUUID().toString())
+                 .property("storageId", "storage0")
+                 .property("repositoryId", "releases")
+                 .as(alias)
+                 .addE(Edges.ARTIFACT_ARTIFACTCOORDINATES)
+                 .to(coordinatesAslias)
+                 .select(alias);
     }
 
 }
